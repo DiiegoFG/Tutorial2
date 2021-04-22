2082d82b4bdf772a7e148d0fc0ccf3cd4876e69d
seedpower55 <83008894+seedpower55@users.noreply.github.com>
2021-04-22 12:23:54 +0200

    crear una variable

--- src/tutorial_entornos/Tutorial_Entornos.java
+++ src/tutorial_entornos/Tutorial_Entornos.java
@@ -15,7 +15,7 @@ public class Tutorial_Entornos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        n=3;
     }
     
 }
